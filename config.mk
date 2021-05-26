a19bc6db5e5dd0ee25e8f6fffa9792042df74229
Oleg Jukovec <oleg.jukovec@gmail.com>
2021-05-26 18:00:32 +0300

    Use c17 standart.

--- config.mk
+++ config.mk
@@ -23,7 +23,7 @@ LIBS = -L${X11LIB} -lX11 -lxkbfile -lpthread ${FREETYPELIBS}
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O3 ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c17 -pedantic -Wall -Wno-deprecated-declarations -O3 ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
