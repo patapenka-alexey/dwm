ce16550b4d06a8b2749ed36823a29d8cc60bd9cf
potapenko <alexey.potapenko@visutechsystem.by>
2023-04-05 12:28:34 +0300

    add parameter to touchpad script

--- scripts/touchpad.sh
+++ scripts/touchpad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+echo Pass 0 or 1 as script parameter to 'turn on' or 'turn off' the touchpad
+
 id=$(xinput | grep Touchpad | awk '{print $5}' | sed "s/id=//")
 
-xinput set-prop $id 'Device Enabled' 0
+xinput set-prop $id 'Device Enabled' $1
