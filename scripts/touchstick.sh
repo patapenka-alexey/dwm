bdbc957b9e2eea7c74fc74276b7dca2f05796552
Alexey Potapenko <potapenkoaleksei@outlook.com>
2023-05-12 18:22:25 +0300

    update touchstick.sh to automatically search id into different names of the devise

--- scripts/touchstick.sh
+++ scripts/touchstick.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -ex
 
-id=$(xinput | grep TrackPoint | awk '{print $5}' | sed "s/id=//")
+# Lenovo ThinkPad Compact USB Keyboard with TrackPoint      id=12   [slave  pointer  (2)]
 
-xinput --set-prop $id 'libinput Accel Speed' -0.35
+device=$(xinput | grep TrackPoint)
+
+for var in $device
+do
+    var_id=${var:0:2}
+    if [ "x$var_id" = "xid" ]
+    then
+        device_number=${var:3:2}
+        echo TrackPoint device number: $device_number
+        xinput --set-prop $device_number 'libinput Accel Speed' -0.35
+        echo Set 'libinput Accel Speed' to '-0.35'
+        break
+    fi
+done
