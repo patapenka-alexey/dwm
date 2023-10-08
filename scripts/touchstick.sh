2dbd9d54cae40cc44c57e5376c2c22993d984d2d
Alexey Potapenko <potapenkoaleksei@outlook.com>
2023-10-08 14:48:59 +0300

    cosmetic

--- scripts/touchstick.sh
+++ scripts/touchstick.sh
@@ -2,7 +2,16 @@
 
 # Lenovo ThinkPad Compact USB Keyboard with TrackPoint      id=12   [slave  pointer  (2)]
 
+echo Pass value of the Speed as script parameter, or default value '-0.35' will be applied
+
 device=$(xinput | grep TrackPoint)
+
+value=-0.35
+
+if [ "x$1" != "x" ]; then
+    value=$1
+fi
+echo Apply value "$value"
 
 for var in $device
 do
@@ -10,8 +19,8 @@ do
     if [ "x$var_id" = "xid" ]; then
         device_number=${var:3:2}
         echo TrackPoint device number: $device_number
-        xinput --set-prop $device_number 'libinput Accel Speed' -0.35
-        echo Set 'libinput Accel Speed' to '-0.35'
+        xinput --set-prop $device_number 'libinput Accel Speed' $value
+        echo Set 'libinput Accel Speed' to $value
         break
     fi
 done
