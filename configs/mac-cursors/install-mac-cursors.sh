bd1ca614a0733e67c1ff5771f8fea004f8befa87
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-01-19 13:32:13 +0300

    update cursor installation instruction

--- configs/mac-cursors/install-mac-cursors.sh
+++ configs/mac-cursors/install-mac-cursors.sh
@@ -7,3 +7,8 @@ sudo update-alternatives --install x-cursor-theme \
 sudo update-alternatives --auto x-cursor-theme
 
 sudo rm -rf /usr/share/icons/mac-cursors
+
+# or copy mac-cursor dir to /usr/share/icons/
+# sudo cp -r ./mac-cursors /usr/share/icons/mac-cursors
+# and update cache
+# sudo update-icon-caches /usr/share/icons/*
