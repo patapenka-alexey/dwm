1fda9726c9176d2988630a421272b6056ac5bef4
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-02-04 21:11:20 +0300

    terminator: toggle maximize by crtl+shift+m

--- configs/moc/mocp_install.sh
+++ configs/moc/mocp_install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$( dirname -- "$BASH_SOURCE"; )";
+
 sudo apt-get install moc
 
 sudo mkdir -p /usr/share/moc/themes
-sudo cp ./themes/* /usr/share/moc/themes
+sudo cp $SCRIPT_DIR/themes/* /usr/share/moc/themes
 
 mkdir ~/.moc/
-cp ./config ~/.moc/config
+cp $SCRIPT_DIR/config ~/.moc/config
